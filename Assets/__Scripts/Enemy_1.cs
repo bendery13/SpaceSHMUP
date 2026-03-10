bdbcd2a2c09dcd9453dbfaddfc538421a11bca69
Ryan Bender <152329584+bendery13@users.noreply.github.com>
2026-03-10 17:16:54 -0500

    Added enemy 1 (pg 819)

--- Assets/__Scripts/Enemy_1.cs
+++ Assets/__Scripts/Enemy_1.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_1 : MonoBehaviour
+public class Enemy_1 : Enemy
 {
-    // Start is called before the first frame update
+    [Header("Enemy_1 Inscribed Fields")]
+    [Tooltip("# of seconds for a full sine wave")]
+    public float waveFrequency = 2;
+    [Tooltip("Sine wave width in meters")]
+    public float waveWidth = 4;
+    [Tooltip("Amount the ship will roll left anf right with the sine wave")]
+    public float waveRotY = 45;
+
+    private float x0; // The initial x value of the Enemy_1
+    private float birthTime;
+
     void Start()
     {
-        
+        x0 = pos.x;
+        birthTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Override the Move function on Enemy
+    public override void Move()
     {
-        
+        // Because pos is a property, we can't directly modify pos.x. We need to use a temporary variable
+        Vector3 tempPos = pos;
+        // Adjust the y position based on the speed
+        float age = Time.time - birthTime;
+        float theta = Mathf.PI * 2 * age / waveFrequency; // Goes from 0 to 2PI every waveFrequency seconds
+        float sin = Mathf.Sin(theta);
+        tempPos.x = x0 + waveWidth * sin;
+        pos = tempPos;
+
+        // rotate a bit around y
+        Vector3 rot = new Vector3(0, waveRotY * sin, 0);
+        this.transform.rotation = Quaternion.Euler(rot);
+
+        // base.Move() still handles the movement down in y
+        base.Move();
+
+        // print(bndCheck.isOnScreen);
     }
+
 }
