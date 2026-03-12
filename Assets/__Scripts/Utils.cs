7b309a785426b72d9e5eb536649f76edf822f5e8
Jacob Stephens <JacobLStephens@outlook.com>
2026-03-12 16:54:07 -0500

    added 3rd enemy movement, pg.833

---
+++ Assets/__Scripts/Utils.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    static public Vector3 Bezier(float u, params Vector3[] points)
+    {
+        //set up the array
+        Vector3[,] vArr = new Vector3[points.Length, points.Length];
+
+        //fill the last row of vArr witht he elements of vList
+        int r = points.Length - 1;
+        for (int  c = 0; c < points.Length; c++)
+        {
+            vArr[r,c] = points[c];
+        }
+
+        //iterate over remaining rows and interpolate points at each one
+        for (r--; r >=0; r--)
+        {
+            for (int c = 0; c <= r; c++)
+            {
+                vArr[r,c] = Vector3.LerpUnclamped(vArr[r+1,c], vArr[r+1,c+1], u);
+            }
+        }
+        //when complete, vArr[0,0] holds the final value
+        return vArr[0,0];
+    }
+}
