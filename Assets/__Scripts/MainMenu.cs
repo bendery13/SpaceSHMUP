ad986893b2bb71c37162036b2a17d5ea965933d1
Ryan Bender <152329584+bendery13@users.noreply.github.com>
2026-03-23 18:19:30 -0500

    Finished MainMenu

--- Assets/__Scripts/MainMenu.cs
+++ Assets/__Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject MainMenuRoot;
+    [SerializeField] private GameObject ControlsMenu;
+
+    private void Awake()
+    {
+        if (MainMenuRoot == null)
+        {
+            MainMenuRoot = GameObject.Find("MainMenu");
+        }
+
+        if (ControlsMenu == null)
+        {
+            ControlsMenu = GameObject.Find("ControlsMenu");
+        }
+    }
+
+    private void Start()
+    {
+        SetMenuState(showMainMenu: true);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("__Scene_0");
@@ -13,6 +35,33 @@ public class MainMenu : MonoBehaviour
     public void ControlsButton()
     {
         Debug.Log("Controls button clicked");
+        SetMenuState(showMainMenu: false);
+    }
+    
+    public void backToMenu()
+    {
+        SetMenuState(showMainMenu: true);
+    }
+
+    private void SetMenuState(bool showMainMenu)
+    {
+        if (MainMenuRoot == null)
+        {
+            Debug.LogWarning("MainMenuRoot is not assigned on MainMenu.");
+        }
+        else
+        {
+            MainMenuRoot.SetActive(showMainMenu);
+        }
+
+        if (ControlsMenu == null)
+        {
+            Debug.LogWarning("ControlsMenu is not assigned on MainMenu.");
+        }
+        else
+        {
+            ControlsMenu.SetActive(!showMainMenu);
+        }
     }
     public void QuitGame()
     {
