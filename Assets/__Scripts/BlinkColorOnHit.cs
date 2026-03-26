611d1b20f3a5bb9229afef5488bbbfacfb575dbd
Jacob Stephens <JacobLStephens@outlook.com>
2026-03-25 19:09:39 -0500

    finished tutorial

--- Assets/__Scripts/BlinkColorOnHit.cs
+++ Assets/__Scripts/BlinkColorOnHit.cs
@@ -11,6 +11,7 @@ public class BlinkColorOnHit : MonoBehaviour
     [Header("Dynamic")]
     public bool showingColor = false;
     public float blinkCompleteTime;
+    public bool ignoreOnCollisionEnter = false;
 
     private Material[] materials;
     private Color[] originalColors;
@@ -35,6 +36,8 @@ public class BlinkColorOnHit : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
+        if (ignoreOnCollisionEnter) return;
+        
         ProjectileHero p = coll.gameObject.GetComponent<ProjectileHero>();
         if (p != null)
         {
@@ -46,7 +49,7 @@ public class BlinkColorOnHit : MonoBehaviour
         }
     }
 
-    void SetColors()
+    public void SetColors()
     {
         foreach (Material m in materials)
         {
